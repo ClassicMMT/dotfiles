4fc7f4ef7694a4fbd985c7e49e828432129944d5
ClassicMMT <porlovskiy@hotmail.com>
2024-08-15 10:50:55 +1200

    update options.lua

--- .config/nvim/lua/peter/core/options.lua
+++ .config/nvim/lua/peter/core/options.lua
@@ -2,6 +2,9 @@ vim.cmd("let g:netrw_liststyle = 3")
 
 local opt = vim.opt -- for conciseness
 
+-- encoding
+opt.encoding = "utf-8"
+
 -- line numbers
 opt.relativenumber = true
 opt.number = true
@@ -18,6 +21,8 @@ opt.wrap = false
 -- search settings
 opt.ignorecase = true
 opt.smartcase = true
+opt.hlsearch = false
+opt.incsearch = true
 
 -- cursor line
 opt.cursorline = true
@@ -40,3 +45,25 @@ opt.splitbelow = true
 -- considers the dash as part of a word
 opt.iskeyword:append("-")
 
+-- scrolling
+opt.scrolloff = 8 -- minimum number of lines above and below cursor when scrolling
+opt.sidescrolloff = 8 -- minimum number of columns
+
+-- python formatting
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+    pattern = "*.py",
+    callback = function()
+        opt.textwidth = 79
+        opt.colorcolumn = "79"
+    end
+})
+
+-- R formatting
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+    pattern = {"*.R", "*.Rmd"},
+    callback = function()
+        opt.tabstop = 2
+        opt.shiftwidth = 2
+        opt.softtabstop = 2
+    end
+})
