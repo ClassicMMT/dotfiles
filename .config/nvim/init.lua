a98aa1297ce0b1a303a6993282a391f7984e4045
ClassicMMT <porlovskiy@hotmail.com>
2024-12-17 08:06:50 +1300

    enter command mode wih ;

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -37,6 +37,7 @@ local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", ";", ":", { desc = "Enter command mode" })
 
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
