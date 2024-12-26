fdc6ed0a2719281fcb6d05c07386c33f74f15a4a
ClassicMMT <porlovskiy@hotmail.com>
2024-12-27 09:59:01 +1300

    update basic config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -62,5 +62,12 @@ local options = vim.opt
 options.scrolloff = 8
 options.sidescrolloff = 8
 
-options.iskeyword:append("-")
+-- wait until timeout
+options.timeoutlen = 250
 
+-- relative number
+options.relativenumber = true
+options.number = true
+
+-- make "-" to be considered part of a word
+options.iskeyword:append("-")
