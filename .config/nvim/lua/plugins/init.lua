8bc1e84ba44522d4db9ae68c81adfe18f32ae6e3
ClassicMMT <porlovskiy@hotmail.com>
2025-01-02 13:04:50 +1300

    treesitter config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -5,7 +5,6 @@ return {
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -13,25 +12,31 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 		  "vim", "lua", "vimdoc",
-  --       -- "html", "css", "bash",
-  --       "markdown", "json", "zsh",
-  --       "gitignore", "python", "r",
-  -- 		},
-  -- 	},
-  --
-  --   indent =  { enable = true, },
-  --
-  --   highlight = {
-  --     enable = true,
-  --     use_languagetree = true,
-  --   },
-  --
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "bash",
+        "markdown",
+        "json",
+        "gitignore",
+        "python",
+        "r",
+      },
+    },
+
+    indent = { enable = true },
+
+    highlight = {
+      enable = true,
+      use_languagetree = true,
+    },
+  },
 
   -- CUSTOM
   {
