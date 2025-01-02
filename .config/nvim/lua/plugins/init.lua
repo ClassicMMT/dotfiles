282c01567761fcbff0210156e2ae4269a532a08e
ClassicMMT <porlovskiy@hotmail.com>
2025-01-02 16:21:33 +1300

    snippets

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -8,25 +8,81 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path", -- source for file system paths
+
+      -- command line autocompletion
       {
         "hrsh7th/cmp-cmdline",
         event = "CmdlineEnter",
         config = function()
           local cmp = require "cmp"
 
-          cmp.setup.cmdline("/", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = { { name = "buffer" } },
-          })
-
           cmp.setup.cmdline(":", {
-            mapping = cmp.mapping.preset.cmdline(),
+            mapping = cmp.mapping.preset.cmdline(), -- custom mappings table goes here
             sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
             matching = { disallow_symbol_nonprefix_matching = false },
           })
         end,
       },
+
+      -- snippets
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
+      },
+      "saadparwaiz1/cmp_luasnip", -- for autocompletion
+      "rafamadriz/friendly-snippets", -- useful snippets
+      "onsails/lspkind.nvim", -- vscode-like pictograms
     },
+
+    config = function()
+      local cmp = require "cmp"
+      local luasnip = require "luasnip"
+      local lspkind = require "lspkind"
+
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup {
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
+
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+
+        mapping = cmp.mapping.preset.insert {
+          ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+          ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+          ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+          ["<CR>"] = cmp.mapping.confirm { select = false },
+        },
+
+        sources = cmp.config.sources {
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- snippets
+          { name = "buffer" }, -- text within current buffer
+          { name = "path" }, -- file system paths
+        },
+
+        -- configure lspkind for vscode-like pictograms in completion menu
+        formatting = {
+          fiels = { "menu", "abbr", "kind" },
+          format = lspkind.cmp_format {
+            maxwidth = 50,
+            ellipsis_char = "...",
+          },
+          expandable_indicator = true,
+        },
+      }
+    end,
   },
 
   {
@@ -62,7 +118,6 @@ return {
     },
   },
 
-  -- CUSTOM
   {
     "max397574/better-escape.nvim",
     config = function()
