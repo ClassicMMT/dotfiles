d3b47224b57c534b3f236c7f3ace250f3f8e5244
ClassicMMT <porlovskiy@hotmail.com>
2025-01-02 16:57:14 +1300

    fix autotag

--- .config/nvim/lua/plugins/nvim-ts-autotag.lua
+++ .config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,15 +1,19 @@
 return {
   "windwp/nvim-ts-autotag",
-  opts = {
-    enable_close = true, -- auto close tags
-    enable_rename = true, -- auto rename pairs of tags
-    enable_close_on_slash = false, -- auto close on trailing </
-  },
-
-  -- customise each filetype separately
-  per_filetype = {
-    ["html"] = {
-      enable_close_on_slash = true,
-    },
-  },
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("nvim-ts-autotag").setup {
+      opts = {
+        enable_close = true, -- auto close tags
+        enable_rename = true, -- auto rename pairs of tags
+        enable_close_on_slash = false, -- auto close on trailing </
+      },
+      -- customise each filetype separately
+      per_filetype = {
+        ["html"] = {
+          enable_close_on_slash = true,
+        },
+      },
+    }
+  end,
 }
