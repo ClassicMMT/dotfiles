dd8b2a7949325892e1a0d333538991764fe984f6
ClassicMMT <porlovskiy@hotmail.com>
2025-01-19 19:26:14 +1300

    none-ls add mypy support

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,21 +1,48 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+    "jayp0521/mason-null-ls.nvim",
+  },
   ft = { "python" },
-  opts = function()
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-    local null_ls = require "null-ls"
+  config = function()
+    require("mason-null-ls").setup {
+      ensure_installed = {
+        "ruff",
+        "prettier",
+        "shfmt",
+      },
+      automatic_installation = true,
+    }
 
-    local opts = {
-      sources = {
-        null_ls.builtins.formatting.black,
-        -- null_ls.builtins.diagnostics.ruff,
-        null_ls.builtins.diagnostics.mypy.with {
-          extra_args = function()
-            local virtual = os.getenv "VIRTUAL_ENV" or os.getenv "CONDA_PREFIX" or "/usr"
-            return { "--python-executable", virtual .. "/bin/python3" }
-          end,
+    local null_ls = require "null-ls"
+    local sources = {
+      -- sorts imports
+      require("none-ls.formatting.ruff").with { extra_args = { "--extend-select", "I" } },
+      -- formats the rest of the file
+      require "none-ls.formatting.ruff_format",
+      null_ls.builtins.formatting.prettier.with {
+        filetypes = {
+          "json",
+          "yaml",
+          "markdown",
         },
       },
+      -- format shell scripts with 4-space indentations
+      null_ls.builtins.formatting.shfmt.with { args = { "-i", "4" } },
+      null_ls.builtins.formatting.black,
+      null_ls.builtins.diagnostics.mypy.with {
+        extra_args = function()
+          local virtual = os.getenv "VIRTUAL_ENV" or os.getenv "CONDA_PREFIX" or "/usr"
+          return { "--python-executable", virtual .. "/bin/python3" }
+        end,
+      },
+    }
+
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    null_ls.setup {
+      -- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog
+      sources = sources,
       on_attach = function(client, bufnr)
         if client.supports_method "textDocument/formatting" then
           vim.api.nvim_clear_autocmds {
@@ -26,12 +53,11 @@ return {
             group = augroup,
             buffer = bufnr,
             callback = function()
-              vim.lsp.buf.format { bufnr = bufnr }
+              vim.lsp.buf.format { async = false, bufnr = bufnr }
             end,
           })
         end
       end,
     }
-    return opts
   end,
 }
