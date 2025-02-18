cb2ec010ba221c848b5891f7cdc538c7e3f2810d
ClassicMMT <porlovskiy@hotmail.com>
2025-02-18 13:36:01 +1300

    add autopairs config - needs work

---
+++ .config/nvim/lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,19 @@
+return {
+  -- -- modified from: ~/.local/share/nvim/lazy/NvChad/lua/nvchad
+  -- "windwp/nvim-autopairs",
+  -- config = function(_, opts)
+  --   local autopairs = require("nvim-autopairs").setup(opts)
+  --   local rule = require "nvim-autopairs.rule"
+  --
+  --   autopairs.add_rules {
+  --     -- rule("\\(", "\\)", "tex"),
+  --     rule("$", "$", { "tex", "markdown", "typst" }),
+  --     -- rule("\\[", "\\]", "tex"),
+  --     -- rule("\\{", "\\}", "tex"),
+  --   }
+  --
+  --   -- setup cmp for autopairs
+  --   local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+  --   require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  -- end,
+}
