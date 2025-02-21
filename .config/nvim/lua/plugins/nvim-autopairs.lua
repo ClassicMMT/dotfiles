2ebccaacea3622f194e2b49237c07944e56288f3
ClassicMMT <porlovskiy@hotmail.com>
2025-02-21 20:20:37 +1300

    add tex and typst autopair rules

--- .config/nvim/lua/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,19 +1,24 @@
 return {
-  -- -- modified from: ~/.local/share/nvim/lazy/NvChad/lua/nvchad
-  -- "windwp/nvim-autopairs",
-  -- config = function(_, opts)
-  --   local autopairs = require("nvim-autopairs").setup(opts)
-  --   local rule = require "nvim-autopairs.rule"
-  --
-  --   autopairs.add_rules {
-  --     -- rule("\\(", "\\)", "tex"),
-  --     rule("$", "$", { "tex", "markdown", "typst" }),
-  --     -- rule("\\[", "\\]", "tex"),
-  --     -- rule("\\{", "\\}", "tex"),
-  --   }
-  --
-  --   -- setup cmp for autopairs
-  --   local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-  --   require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  -- end,
+  -- modified from: ~/.local/share/nvim/lazy/NvChad/lua/nvchad
+  "windwp/nvim-autopairs",
+  opts = {
+    fast_wrap = {},
+    disable_filetype = { "TelescopePrompt", "vim" },
+  },
+  config = function(_, opts)
+    require("nvim-autopairs").setup(opts)
+    local rule = require "nvim-autopairs.rule"
+
+    local autopairs = require "nvim-autopairs"
+    autopairs.add_rules {
+      rule("\\(", "\\)", "tex"),
+      rule("$", "$", { "tex", "markdown", "typst" }),
+      rule("\\[", "\\]", "tex"),
+      rule("\\{", "\\}", "tex"),
+    }
+
+    -- setup cmp for autopairs
+    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+    require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
 }
