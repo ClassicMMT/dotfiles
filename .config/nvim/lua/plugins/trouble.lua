f23cc5ea14352244682a820f7b8707355da39b78
ClassicMMT <porlovskiy@hotmail.com>
2025-03-08 20:16:02 +1300

    add setup

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -2,18 +2,26 @@ return {
   -- "folke/trouble.nvim",
   -- dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
   -- opts = {
-  -- 	focus = true,
+  --   focus = true,
+  --   win = { "floating" },
   -- },
   -- cmd = "Trouble",
   -- keys = {
-  -- 	{ "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", desc = "Trouble Open trouble workspace diagnostics" },
-  -- 	{
-  -- 		"<leader>xd",
-  -- 		"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
-  -- 		desc = "Trouble Open document diagnostics",
-  -- 	},
-  -- 	{ "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Trouble Open quickfix list" },
-  -- 	{ "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Trouble Open location list" },
-  -- 	{ "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Trouble Open todos" },
+  --   { "<leader>to", "<cmd>Trouble diagnostics toggle<CR>", desc = "Trouble Open trouble workspace diagnostics" },
+  --   {
+  --     "<leader>xd",
+  --     "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+  --     desc = "Trouble Open document diagnostics",
+  --   },
+  --   { "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Trouble Open quickfix list" },
+  --   { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Trouble Open location list" },
+  --   { "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Trouble Open todos" },
   -- },
+  -- config = function()
+  --   local todo = require "todo-comments"
+  --   local trouble = require "trouble"
+  --
+  --   trouble.setup {}
+  --   todo.setup {}
+  -- end,
 }
