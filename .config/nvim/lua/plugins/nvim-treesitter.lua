5b142a51a7dd5a0bd908d66cf1b5c2cade302f36
ClassicMMT <porlovskiy@hotmail.com>
2025-03-08 20:14:26 +1300

    clean up

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -55,26 +55,31 @@ return {
 
         keymaps = {
           -- You can use the capture groups defined in textobjects.scm
+          -- assignments
           ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
           ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
           ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
           ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
 
-          -- works for javascript/typescript files (custom capture I created in after/queries/ecma/textobjects.scm)
-          ["a:"] = { query = "@property.outer", desc = "Select outer part of an object property" },
-          ["i:"] = { query = "@property.inner", desc = "Select inner part of an object property" },
-          ["l:"] = { query = "@property.lhs", desc = "Select left part of an object property" },
-          ["r:"] = { query = "@property.rhs", desc = "Select right part of an object property" },
+          -- -- works for javascript/typescript files (custom capture I created in after/queries/ecma/textobjects.scm)
+          -- ["a:"] = { query = "@property.outer", desc = "Select outer part of an object property" },
+          -- ["i:"] = { query = "@property.inner", desc = "Select inner part of an object property" },
+          -- ["l:"] = { query = "@property.lhs", desc = "Select left part of an object property" },
+          -- ["r:"] = { query = "@property.rhs", desc = "Select right part of an object property" },
 
+          -- arguments
           ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
           ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
 
+          -- conditions
           ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
           ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
 
+          -- loops
           ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
           ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
 
+          -- functions
           ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
           ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
 
