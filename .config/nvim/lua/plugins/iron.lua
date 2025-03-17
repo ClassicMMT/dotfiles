071d7b941950e89eec4e1d02de1b5e128d854ab6
ClassicMMT <porlovskiy@hotmail.com>
2025-03-17 19:07:15 +1300

    fix CR mapping bugs

--- .config/nvim/lua/plugins/iron.lua
+++ .config/nvim/lua/plugins/iron.lua
@@ -8,15 +8,16 @@ return {
     { "<leader>rh", "<cmd>IronHide<CR>", desc = "Hide iron" },
   },
   config = function()
-    local iron = require "iron.core"
+    local core = require "iron.core"
     local view = require "iron.view"
 
-    iron.setup {
+    core.setup {
       config = {
+        close_window_on_exit = true,
         highlight_last = false,
         scratch_repl = true,
-        -- repl_open_cmd = view.split.vertical.botright(0.5),
-        repl_open_cmd = view.bottom(0.3),
+        repl_open_cmd = view.split.vertical.botright(0.3),
+        -- repl_open_cmd = view.bottom(0.3),
         -- preferred = {
         --   python = "python"
         -- },
@@ -24,7 +25,13 @@ return {
           sh = { "zsh" },
           python = {
             -- command = { "python" },
-            command = { "ipython" },
+            command = {
+              "ipython",
+              "--no-autoindent",
+              "--colors=linux", -- NoColor, Neutral, Linux, or LightBG
+              "--no-confirm-exit",
+              "--color-info",
+            },
             -- run "pip install ptpython" - otherwise, use the line above
             -- command = { "ptpython", "--vi" },
             format = require("iron.fts.common").bracketed_paste,
@@ -35,18 +42,18 @@ return {
       keymaps = {
         -- send_motion = "<leader>sc",
         -- visual_send = "<leader>sc",
-        visual_send = "<CR>",
+        visual_send = "<leader>sl",
         send_file = "<leader>sf",
         -- send_line = "<leader>sl",
-        send_line = "<CR>",
+        send_line = "<leader>sl",
         send_until_cursor = "<leader>su",
         -- send_mark = "<leader>sm",
         -- mark_motion = "<leader>mc",
         -- mark_visual = "<leader>mc",
         -- remove_mark = "<leader>md",
         -- cr = "<leader>s<cr>",
-        interrupt = "<leader>s<leader>",
-        exit = "<leader>sq",
+        interrupt = "<leader>ri",
+        exit = "<leader>rq",
         clear = "<leader>cl",
       },
       highlight = {
@@ -54,5 +61,27 @@ return {
       },
       ignore_blank_lines = true,
     }
+
+    -- make the CR mapping function properly
+    local map = vim.keymap.set
+
+    map("n", "<CR>", function()
+      -- safely call core.send_line
+      local status, _ = pcall(core.send_line)
+      -- if the call fails, then simulate a standard CR press
+      if not status then
+        -- "\r" is the <CR> key
+        vim.api.nvim_feedkeys("\r", "n", true)
+        -- return vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", true)
+      end
+    end, { noremap = true, silent = true })
+
+    map("v", "<CR>", function()
+      local status, _ = pcall(core.visual_send)
+      if not status then
+        vim.api.nvim_feedkeys("\r", "n", true)
+        -- return vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", true)
+      end
+    end, { noremap = true, silent = true })
   end,
 }
