6f2c1ffbf7581c0a3f8da958d2d467155cd90cb1
ClassicMMT <porlovskiy@hotmail.com>
2025-03-22 15:43:42 +1300

    make iron repl sending behaviour consistent with r.nvim

--- .config/nvim/lua/plugins/iron.lua
+++ .config/nvim/lua/plugins/iron.lua
@@ -1,12 +1,13 @@
 return {
   "Vigemus/iron.nvim",
   cmd = { "IronRepl" },
-  keys = {
-    { "<leader>rs", "<cmd>IronRepl<CR>", desc = "Start iron" },
-    { "<leader>rr", "<cmd>IronRestart<CR>", desc = "Restart iron" },
-    { "<leader>rf", "<cmd>IronFocus<CR>", desc = "Focus iron" },
-    { "<leader>rh", "<cmd>IronHide<CR>", desc = "Hide iron" },
-  },
+  ft = { "python" },
+  -- keys = {
+  --   { "<leader>rs", "<cmd>IronRepl<CR>", desc = "Start iron" },
+  --   { "<leader>rr", "<cmd>IronRestart<CR>", desc = "Restart iron" },
+  --   { "<leader>rf", "<cmd>IronFocus<CR>", desc = "Focus iron" },
+  --   { "<leader>rh", "<cmd>IronHide<CR>", desc = "Hide iron" },
+  -- },
   config = function()
     local core = require "iron.core"
     local view = require "iron.view"
@@ -62,8 +63,15 @@ return {
       ignore_blank_lines = true,
     }
 
-    -- make the CR mapping function properly
+    -- general mappings
     local map = vim.keymap.set
+
+    map("n", "<leader>rs", "<cmd>IronRepl<CR>", { desc = "Start iron" })
+    map("n", "<leader>rr", "<cmd>IronRestart<CR>", { desc = "Restart iron" })
+    map("n", "<leader>rf", "<cmd>IronFocus<CR>", { desc = "Focus iron" })
+    map("n", "<leader>rh", "<cmd>IronHide<CR>", { desc = "Hide iron" })
+
+    -- functions for sanity
 
     local function call_with_fallback(f, fallback)
       -- safely call f
@@ -76,6 +84,7 @@ return {
       end
     end
 
+    -- function for finding and moving to the next non-blank line
     local function move_to_next_non_blank_line(end_line)
       local line_number = end_line or vim.fn.line "."
       local line_count = vim.api.nvim_buf_line_count(0)
@@ -93,23 +102,119 @@ return {
       end
     end
 
+    local function is_allowed_type(type)
+      local allowed_types = {
+        "expression_statement",
+        "function_definition",
+        "class_definition",
+      }
+      for _, v in ipairs(allowed_types) do
+        if v == type then
+          return true
+        end
+      end
+      return false
+    end
+
+    local function find_top_level_allowed_node(node)
+      local parent = node:parent()
+      local highest_node = nil
+      while parent do
+        local node_type = node:type()
+        -- print(node_type, is_allowed_type(node_type))
+        if is_allowed_type(node_type) then
+          highest_node = node
+        end
+        node = parent
+        parent = node:parent()
+      end
+      return highest_node
+    end
+
+    -- function for finding and selecting an expression statement
+    -- to be used in before call_with_fallback(core.visual_send, FALLBACK)
+    local function get_top_level_node_range()
+      -- nothing
+      local utils = require "nvim-treesitter.ts_utils"
+      local node = utils.get_node_at_cursor()
+
+      -- find the top-level node
+      local top_level_node = find_top_level_allowed_node(node)
+      if top_level_node then
+        return true, { top_level_node:range() }
+      else
+        return false, nil
+      end
+
+      -- while node do
+      --   local node_type = node:type()
+      --   if
+      --     node_type == "expression_statement"
+      --     or node_type == "function_definition"
+      --     or node_type == "class_definition"
+      --   then
+      --     -- get node range
+      --     return true, { node:range() }
+      --   end
+      --   -- move to parent node
+      --   node = node:parent()
+      -- end
+      -- -- top node is not an expression statement
+      -- return false
+    end
+
+    local function select_expression_statement(range)
+      -- local unpack = unpack or table.unpack
+      local start_row, start_col, end_row, end_col = unpack(range)
+      -- set visual selection range
+      vim.api.nvim_win_set_cursor(0, { start_row + 1, start_col })
+      vim.cmd "normal! v"
+      -- end_col needs to be adjusted, otherwise it selects one too many
+      vim.api.nvim_win_set_cursor(0, { end_row + 1, end_col - 1 })
+      -- expression statement found
+      return true
+    end
+
+    local function get_cursor_position()
+      -- returns the position of the cursor
+      return vim.api.nvim_win_get_cursor(0)
+    end
+
+    local function restore_cursor_position(row, column)
+      vim.api.nvim_win_set_cursor(0, { row, column })
+    end
+
     map("n", "<CR>", function()
-      call_with_fallback(core.send_line)
+      local found, range = get_top_level_node_range()
+      if found then
+        -- save the cursor position
+        local cursor_row, cursor_column = unpack(get_cursor_position())
+        select_expression_statement(range)
+        call_with_fallback(core.visual_send)
+        restore_cursor_position(cursor_row, cursor_column)
+      else
+        call_with_fallback(core.send_line)
+      end
+    end, { noremap = true, silent = true })
+
+    map("n", "<S-CR>", function()
+      local found, range = get_top_level_node_range()
+      if found then
+        select_expression_statement(range)
+        call_with_fallback(core.visual_send, "<S-CR>")
+      else
+        call_with_fallback(core.send_line)
+      end
+      move_to_next_non_blank_line()
     end, { noremap = true, silent = true })
 
     map("v", "<CR>", function()
       call_with_fallback(core.visual_send)
     end, { noremap = true, silent = true })
 
-    map("n", "<S-CR>", function()
-      call_with_fallback(core.send_line, "<S-CR>")
-      move_to_next_non_blank_line()
-    end, { noremap = true, silent = true })
-
     map("v", "<S-CR>", function()
       call_with_fallback(core.visual_send, "<S-CR>")
       move_to_next_non_blank_line(vim.fn.getpos("'>")[2])
-      -- move_to_next_non_blank_line()
     end, { noremap = true, silent = true })
   end,
 }
