5d4afaf21969cc869dcdfb995af12b98518a1288
ClassicMMT <porlovskiy@hotmail.com>
2025-08-11 20:26:55 +1200

    disable autopairs

--- .config/nvim/lua/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,4 +1,4 @@
-local cmp = require "cmp"
+-- local cmp = require "cmp"
 return {
   -- modified from: ~/.local/share/nvim/lazy/NvChad/lua/nvchad
   "windwp/nvim-autopairs",
@@ -18,24 +18,24 @@ return {
       rule("\\{", "\\}", "tex"),
     }
 
-    -- setup cmp for autopairs
-    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-    require("cmp").event:on(
-      "confirm_done",
-      cmp_autopairs.on_confirm_done {
-        -- custom config - stops autopairs from inserting brackets after function calls in python
-        filetypes = {
-          ["python"] = {
-            ["("] = {
-              kind = {
-                cmp.lsp.CompletionItemKind.Function,
-                cmp.lsp.CompletionItemKind.Method,
-              },
-              handler = function() end,
-            },
-          },
-        },
-      }
-    )
+    --   -- setup cmp for autopairs
+    --   local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+    --   require("cmp").event:on(
+    --     "confirm_done",
+    --     cmp_autopairs.on_confirm_done {
+    --       -- custom config - stops autopairs from inserting brackets after function calls in python
+    --       filetypes = {
+    --         ["python"] = {
+    --           ["("] = {
+    --             kind = {
+    --               cmp.lsp.CompletionItemKind.Function,
+    --               cmp.lsp.CompletionItemKind.Method,
+    --             },
+    --             handler = function() end,
+    --           },
+    --         },
+    --       },
+    --     }
+    --   )
   end,
 }
