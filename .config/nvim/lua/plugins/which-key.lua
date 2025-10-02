babad178c4383b3eb80574a9fd632300a3faedad
ClassicMMT <porlovskiy@hotmail.com>
2025-10-02 20:54:56 +1300

    remove which key for marks

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -7,5 +7,12 @@ return {
   end,
   opts = function()
     dofile(vim.g.base46_cache .. "whichkey")
+
+    -- don't show which-key for marks
+    return {
+      plugins = {
+        marks = false,
+      },
+    }
   end,
 }
